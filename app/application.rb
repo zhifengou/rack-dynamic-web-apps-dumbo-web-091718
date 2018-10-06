1561ece7b26d693ed6af53a38eebc5fd69a86422
zhifeng ou <zhifengou@hotmail.com>
2018-10-06 17:32:50 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -10,7 +10,7 @@ class Application
     resp.write "#{num_2}\n"
     resp.write "#{num_3}\n"
     
-    if num_1==num_2 && num_2 == num_3
+    if num_1 == num_2 && num_2 == num_3
       resp.write "You Win"
     else 
       resp.write "You Lose"
